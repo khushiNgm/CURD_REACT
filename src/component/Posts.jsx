f05c219ba2291b9fe9c7183d289ed325857dfc57
Khushi Nigam <161741565+khushiNgm@users.noreply.github.com>
2025-03-06 09:33:12 +0530

    Update Posts.jsx

--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -5,9 +5,7 @@ import Form from './Form';
 export const Posts =()=>{
 
     const[data,setData] = useState([]);
-
       console.log( getPost () );
-        
         const getPostData = async () =>{
         const res= await getPost();
         console.log(res.data);
@@ -39,14 +37,15 @@ export const Posts =()=>{
               };
       return (
         <>
-            <section><Form data={data} setData={setData}/></section>
+            <section className="ml-[50px] sm:px-6 md:px-8"><Form data={data} setData={setData}/></section>
      <section className='w-full max-w-screen-lg mx-auto mt-6'>
       <ol className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 list-decimal list-inside text-white'>
          {data.map((curElm) => {
           const { id, title, body } = curElm;
           return (
+            <div  className='ml-[30px]'>
             <li key={id} className='bg-blue-950 rounded-lg p-4 shadow-black shadow-lg border-2 border-white'>
-              <p className='text-white'>Title: {title}</p>
+              <p className='text-white sm:text-base md:text-lg font-semibold'>Title: {title}</p>
               <p className='text-white'>Body: {body}</p>
               <div className='flex justify-center gap-4 mt-3 mb-3'>
                 <button className='bg-blue-500 border-2 border-gray-300 text-white px-4 py-2 hover:bg-green-400 rounded-xl'>Edit</button>
@@ -58,6 +57,7 @@ export const Posts =()=>{
                 </button>
               </div>
             </li>
+             </div>
           );
         })}
       </ol>
