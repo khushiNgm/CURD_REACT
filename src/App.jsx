4bf2e045827cae7a7c67f872c1dbd5280c68c99e
Khushi Nigam <161741565+khushiNgm@users.noreply.github.com>
2026-03-05 22:58:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <section className="flex justify-center w-screen h-auto flex-col items-center mt-10">
       <div className="p-4">
       <h1 className="text-white text-5xl text-center text-bold">
-     CRUD operation<span className="block">with React</span>
+     CRUD operation<span className="block">with React by khushi :) </span>
     </h1>
       </div>
     <Posts/>
